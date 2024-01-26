a252edeabd3d055dffa2f636193513bb3f3f06e5
Darya and Code story <daryaandcodestory@Fiodars-MacBook-Pro-4.local>
2024-01-26 11:25:41 -0500

    new commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,31 @@
-import Calculator from "./components/Calculator"
+import { useState } from 'react';
+
+import UserInput from "./components/UserInput"
+import Results from './components/Results';
 
 
 function App() {
+  const[userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
+
+  function handleChange(inputIdentifier, newValue){
+    setUserInput(prevInput => {
+      return {
+        ...prevInput, 
+        [inputIdentifier]: newValue
+      };
+    });
+  }
+   
+
   return (
     <>
-    <main id='input-group'>
-    <Calculator title='input1'description='test'/>
-    <Calculator/>
-    <Calculator/>
-    <Calculator/>
-    OUTPUT
-    </main>
+    <UserInput userInput={userInput} onChange={handleChange}/>
+    <Results input={userInput}/>
     </>
 
   )
--- src/components/Calculator.jsx
+++
@@ -1,9 +0,0 @@
-import { useState } from "react"
-
-export default function Calculator(){
-  return(
-    <tr id='user-input'>
-      <input></input>
-    </tr>
-      )
-}
